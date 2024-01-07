4cb0464eb7a4a55144abc56b02d282b413988a0e
mfidanci <mfidanci93@ya>
2024-01-07 14:18:49 +0300

    Django Projesi oluşturma    --venv oluşturma ve aktif etme Uygulama (app) Oluşturma Model Tanımlama (models.py) Admin sitesi yapılandırma URL Tanımlama (Urls.py) View fonksiyonu yazma (views.py) Şablon (Templates) oluşturma   - HTML   _ Şablon etiketleri{% for ....%}   - Şablon Mirası(base.html) Migration yapma4 Ayarlar dosyası (setting.py)

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .models import Topics
+
 # Create your views here.
+def index(request):
+    """The homepage for Learning Log. """
+    return render(request, template_name='learning_logs/index.html')
+
+def topics(request):
+    topics = Topics.objects.all()
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Show a single topic and all its entries."""
+    topic = Topics.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
